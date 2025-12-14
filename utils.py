d8ce0e575699fd5bed1abd6c2c8fcfbdb51abbb5
Raniz85 <raniz+github@raneland.se>
2025-12-14 16:21:16 +0000

    Clear out utils.py, add autoreload to all notebooks

--- utils.py
+++
@@ -1,8 +0,0 @@
-import os.path
-
-# Define a helper to load the input for a series
-def load_input(part: int) -> [str]:
-    """Loads the input for a part of the kata from the volume"""
-    input_path = os.path.join(VOLUME_PATH, f"./series{part:02d}.txt")
-    with open(input_path) as f:
-        return f.readlines()
